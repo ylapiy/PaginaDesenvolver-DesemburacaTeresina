22dfb5bc9536d30c11c6de4ce09003339dcbfbb1
ylapiy <ygorgivagooo@gmail.com>
2025-06-20 20:38:39 -0300

    agora vai

---
+++ pagina/scriptdosamigos.js
@@ -0,0 +1,50 @@
+fetch('https://projetodesemburacateresinaapi-production-1abf.up.railway.app/registro') 
+  .then(response => response.json())
+  .then(data => {
+    const tbody = document.querySelector("#tabelaDenuncias tbody");
+    data.forEach(registro => {
+      const linha = `
+        <tr>
+          <td>${registro.fid}</td>
+          <td>${registro.data}</td>
+          <td>${registro.categoria}</td>
+          <td>${registro.status}</td>
+          <td>${registro.observacao}</td>
+          <td>${registro.rua}</td>
+          <td>${registro.bairro}</td>
+
+          <button id="excluir">Excluir</button>
+          <button>Editar</button>
+
+        
+   
+        </tr>
+      `;
+      tbody.innerHTML += linha;
+      document.getElementById('excluir').addEventListener('click', ()=>{
+         DeletBancoDeDados(registro.fid)
+      })
+    });
+  });
+
+  async function DeletBancoDeDados(id){
+
+    fetch(`https://projetodesemburacateresinaapi-production-1abf.up.railway.app/registro/${id}`, {method:'DELETE'})
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data)
+
+    })
+  
+  }
+
+
+
+
+  async function MudarBancoDeDados() {
+    
+
+
+
+
+  }
